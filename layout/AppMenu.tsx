cd8f2d3693d0f5f601b7cbe1f86d4fcf3230edc3
Developer <developer@example.com>
2026-05-26 12:40:32 +0700

    Initial commit after project cleanup

---
+++ layout/AppMenu.tsx
@@ -0,0 +1,61 @@
+import { MenuModal } from '@/types/layout';
+import AppSubMenu from './AppSubMenu';
+
+const AppMenu = () => {
+    const model: MenuModal[] = [
+        {
+            label: 'Dashboards',
+            icon: 'pi pi-home',
+            items: [
+                {
+                    label: 'E-Commerce',
+                    icon: 'pi pi-fw pi-home',
+                    to: '/'
+                }
+            ]
+        },
+        { separator: true },
+        {
+            label: 'User Management',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'List',
+                    icon: 'pi pi-fw pi-list',
+                    to: '/profile/list'
+                },
+                {
+                    label: 'Create',
+                    icon: 'pi pi-fw pi-plus',
+                    to: '/profile/create'
+                }
+            ]
+        },
+        { separator: true },
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            items: [
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+                {
+                    label: 'Empty',
+                    icon: 'pi pi-fw pi-circle-off',
+                    to: '/pages/empty'
+                },
+                {
+                    label: 'Help',
+                    icon: 'pi pi-fw pi-question-circle',
+                    to: '/pages/help'
+                }
+            ]
+        }
+    ];
+
+    return <AppSubMenu model={model} />;
+};
+
+export default AppMenu;
